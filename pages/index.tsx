239d7b585733404eeebffa928dc7e06348115234
Kanstantsin Zanin <insanezorr@mail.ru>
2021-09-14 18:03:49 +0200

    fix: fix prettier index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 import { Button, Htag } from "../components";
 
-
 export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearance='primary' arrow='down'>Click me</Button>
-      <Button appearance='ghost'>Click me</Button>
-      <Button appearance='ghost' arrow='right'>Click me</Button>
+      <Button appearance="primary" arrow="down">
+        Click me
+      </Button>
+      <Button appearance="ghost">Click me</Button>
+      <Button appearance="ghost" arrow="right">
+        Click me
+      </Button>
     </>
   );
 }
