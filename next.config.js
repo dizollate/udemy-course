5cfdc7a16eb000deaa6c9d104a70407aebc4f36d
Kanstantsin Zanin <insanezorr@mail.ru>
2021-09-14 18:02:29 +0200

    feat: add new module.export next.js and create color dependence of props

--- next.config.js
+++ next.config.js
@@ -1,12 +1,35 @@
 module.exports = {
-  webpack(config) {
-      config.module.rules.push({
-          test: /\.svg$/, //Если мы видим файл свг
-          issuer: { //в файла дж, тс, или тсх, 
-              test: /\.(js|ts)x?$/,
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.svg?$/,
+
+      oneOf: [
+        {
+          use: [
+            {
+              loader: "@svgr/webpack",
+
+              options: {
+                prettier: false,
+
+                svgo: true,
+
+                svgoConfig: {
+                  plugins: [{ removeViewBox: false }],
+                },
+
+                titleProp: true,
+              },
+            },
+          ],
+
+          issuer: {
+            and: [/\.(ts|tsx|js|jsx|md|mdx)$/],
           },
-          use: ['@svgr/webpack'], //то мы с вами используем специальный загрузчик svgr 
-      });
-      return config;
+        },
+      ],
+    });
+
+    return config;
   },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home(): JSX.Element {
   return (
     <>
       <Htag tag="h1">Текст</Htag>
-      <Button appearance='primary'>Click me</Button>
+      <Button appearance='primary' arrow='down'>Click me</Button>
       <Button appearance='ghost'>Click me</Button>
       <Button appearance='ghost' arrow='right'>Click me</Button>
     </>
