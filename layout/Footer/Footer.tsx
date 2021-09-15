66b221d646d353caae53866da567d3097fc937d4
Kanstantsin Zanin <insanezorr@mail.ru>
2021-09-15 22:29:31 +0200

    feat: add layout to footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,6 +1,19 @@
-import { IFooter} from "./Footer.props";
+import { IFooter } from "./Footer.props";
+import styles from "./Footer.module.css";
 //import styles from './Footer.module.css';
 
 export const Footer = ({ ...props }: IFooter): JSX.Element => {
-  return <footer {...props}>Footer</footer>;
+  return (
+    <footer {...props}>
+      <div className={styles.content}>
+        <span className={styles.rights}>
+          OwlTop © 2020 - 2021 Все права защищены
+        </span>
+        <div className={styles.agreement_privacy}>
+          <span className={styles.agreement}>Пользовательское соглашение</span>
+          <span className={styles.privacy}>Политика конфиденциальности</span>
+        </div>
+      </div>
+    </footer>
+  );
 };
--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -3,17 +3,16 @@ import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { ILayout } from "./Layout.props";
 import { Sidebar } from "./Sidebar/Sidebar";
+import styles from "./Layout.module.css";
 
 const Layout = ({ children }: ILayout): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <body className={styles.body}>{children}</body>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
