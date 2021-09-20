f3409f19e4e47d860ab09c56c04f43b66fba5d2b
Kanstantsin Zanin <insanezorr@mail.ru>
2021-09-20 13:33:57 +0200

    feat: layot menu

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
 import { Menu } from "../Menu/Menu";
-import { ISidebar} from "./Sidebar.props";
+import { ISidebar } from "./Sidebar.props";
 //import styles from './Sidebar.module.css';
 
 export const Sidebar = ({ ...props }: ISidebar): JSX.Element => {
-  return <section {...props}>
-    <Menu/>
-  </section>;
+  return (
+    <section {...props}>
+      <Menu />
+    </section>
+  );
 };
