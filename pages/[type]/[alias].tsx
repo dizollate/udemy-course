560d419e94bf0003c562b28f0a40ffe99cd89c16
Kanstantsin Zanin <insanezorr@mail.ru>
2021-09-23 14:30:51 +0200

    feat: add yandex metrika

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -11,18 +11,24 @@ import { ProductModel } from "../../interfaces/product.interface";
 import { firstLevelMenu } from "../../helpers/helpers";
 import { TopPageComponent } from "../../page-components";
 import { API } from "../../helpers/api";
+import Head from "next/head";
 
-function TopPage({
-  page,
-  products,
-  firstCategory,
-}: TopPageProps): JSX.Element {
+function TopPage({ page, products, firstCategory }: TopPageProps): JSX.Element {
   return (
-    <TopPageComponent
-      firstCategory={firstCategory}
-      page={page}
-      products={products}
-    />
+    <>
+      <Head>
+        <title>{page.metaTitle}</title>
+        <meta name="description" content={page.metaDescription} />
+        <meta property="og:title" content={page.metaTitle} />
+        <meta property="og:description" content={page.metaDescription} />
+        <meta property='og:type' content='article' />
+      </Head>
+      <TopPageComponent
+        firstCategory={firstCategory}
+        page={page}
+        products={products}
+      />
+    </>
   );
 }
 
@@ -31,12 +37,9 @@ export default WithLayout(TopPage);
 export const getStaticPaths: GetStaticPaths = async () => {
   let paths: string[] = [];
   for (const m of firstLevelMenu) {
-    const { data: menu } = await axios.post<MenuItem[]>(
-      API.topPage.find,
-      {
-        firstCategory: m.id,
-      }
-    );
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+      firstCategory: m.id,
+    });
     paths = paths.concat(
       menu.flatMap((s) => s.pages.map((p) => `/${m.route}/${p.alias}`))
     );
@@ -62,12 +65,9 @@ export const getStaticProps: GetStaticProps<TopPageProps> = async ({
     };
   }
   try {
-    const { data: menu } = await axios.post<MenuItem[]>(
-      API.topPage.find,
-      {
-        firstCategory: firstCategoryItem.id,
-      }
-    );
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+      firstCategory: firstCategoryItem.id,
+    });
     if (menu.length == 0) {
       return {
         notFound: true,
